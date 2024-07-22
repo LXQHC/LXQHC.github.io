7e64888d77fd49c3b6e468d10a20c2592e064d62
LXQHC <1634166194@qq.com>
2024-07-22 23:55:46 +0800

    Site updated: 2024-07-22 23:55:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/22/hello-world/"];function toRandomPost(){
+var posts=["2024/07/22/自媒体小白，两个月破万粉？！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
